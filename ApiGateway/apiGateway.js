2472abdfb8d554d9fef33364e9a1338fd03c35f5
rvb2000 <66408706+rvb2000@users.noreply.github.com>
2020-10-24 12:26:57 +0530

    Add files via upload

---
+++ ApiGateway/apiGateway.js
@@ -0,0 +1,20 @@
+
+
+const gateway = require('fast-gateway');
+
+const server = gateway({
+    routes:[{
+
+        prefix:'/products',
+        target: 'http://localhost:4000'
+    },{
+        prefix:'/orders',
+        target: 'http://127.0.1:6000'
+    },{
+        prefix:'/library',
+        target: 'http://localhost:5000'
+    }]
+})
+server.start(3000);
+console.log("Server running port 3000");
+
